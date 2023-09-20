d7744d5f0701f8a67778d3de576bb8acb0895d04
Tyler Yan <117669511+TyYan03@users.noreply.github.com>
2023-09-20 00:45:24 -0400

    Update activity3.py to include a timestamp on the server

--- activity3.py
+++ activity3.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
-
+moment = Moment(app)
 
 @app.route('/')
 def index():
@@ -13,4 +15,4 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
